b69e54ab16e29ba745671c28069cfc1e6c06a29c
Will Wadman <wadmanwill@gmail.com>
2022-10-25 20:17:16 +0000

    Linting timer #1

--- timer1.js
+++ timer1.js
@@ -3,41 +3,41 @@
  * Our input will be positive intergers passed via CLI
  * Our output will be beeps in VS code
  * We will likely need to loop through the input in an array and use the index to notify the code block to beep
- * 
- * 
+ *
+ *
 
 
 
- * Edge Cases: 
+ * Edge Cases:
  * #1: No numbers are provided
  *      Result: no beeps since none are scheduled
  * #2: Input is a negative number
- *      Result: skip over negative numbers
+ *      Result: skip over negative number
  * #3: Input is not a number
  *      Result: skip over it
  */
 
 const args = process.argv;
-let timesToBeepArray = args.slice(2).sort((a, b) => a -b)
+let timesToBeepArray = args.slice(2).sort((a, b) => a - b);
 
 
-if(!timesToBeepArray.length){
-return;
+if (!timesToBeepArray.length) {
+  return;
 }
 
 for (let beeps of timesToBeepArray) {
- const beepTime = Number(beeps)
- console.log(typeof beepTime)
-  if (!beepTime){
-    console.log("hi")
+  const beepTime = Number(beeps);
+  console.log(typeof beepTime);
+  if (!beepTime) {
+    console.log("hi");
     continue;
   }
-  if (beepTime <= 0){
+  if (beepTime <= 0) {
     continue;
   }
-  let timesInMilli = (beepTime *1000)
+  let timesInMilli = (beepTime * 1000);
   setTimeout(() => {
-    console.log('beep')
+    console.log('beep');
     process.stdout.write('\x07');
-   }, timesInMilli);
-};
+  }, timesInMilli);
+}
